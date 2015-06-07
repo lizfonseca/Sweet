34167843a77c9c05aea12bbe315ed9f923347fb7
Liz Fonseca <lfonsec04@gmail.com>
2015-06-07 18:11:42 -0400

    adds candy into database; country-id bug

--- app/controllers/categories/candies_controller.rb
+++ app/controllers/categories/candies_controller.rb
@@ -5,5 +5,34 @@ module Categories
       @candies = Candy.all
       render :index
     end
+
+    def new
+      @categories = Category.all
+      @countries = Country.all
+      @category = Category.find(params[:category_id])
+      @candy = Candy.new
+
+      render :new
+    end
+
+    def create
+      @countries = Country.all
+      @category = Category.find(params[:category_id])
+      @candy = Candy.new
+
+      @candy.name = params[:name]
+      @candy.company = params[:company]
+      @candy.image_link = params[:image_link]
+      @candy.description = params[:description]
+      @candy.country_id = params[:country_id]
+      @candy.category_id = params[:category_id]
+      @candy.discontinued = params[:discontinued]
+
+      if @candy.save
+        redirect_to category_candies_path
+      else 
+        render :new
+    end
+    end
   end
 end
--- app/controllers/countries/candies_controller.rb
+++ app/controllers/countries/candies_controller.rb
@@ -5,5 +5,10 @@ module Countries
       @candies = Candy.all
       render :index
     end
+    def new
+      @categories = Category.all
+      @countries = Country.all
+      @country = Country.find(params[:country_id])
+    end
   end
 end
